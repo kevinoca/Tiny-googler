baa63861c832da13fa3dedfa390644b38dc9b78f
Kevin Oca <kevinoca1993@gmail.com>
2019-02-26 15:00:38 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const electron = require('electron');  
+const app = electron.app;  
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('window-all-closed', function() {  
+  if(process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {  
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('https://google.es');
+
+  // mainWindow.on('closed', function() {
+  //   mainWindow = null;
+  // });
+});
