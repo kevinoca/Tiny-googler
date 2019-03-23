e32f43a2da84538f1873cf100d019fb90a88eeb8
Kevin Roman Oca Sierra <kevinoca1993@gmail.com>
2019-03-23 22:02:54 +0100

    Disabling Resize events on app.

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 'use strict';
 
-const electron = require('electron');  
-const app = electron.app;  
+const electron = require('electron');
+const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
-app.on('window-all-closed', function() {  
-  if(process.platform != 'darwin') {
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
-app.on('ready', function() {  
-  mainWindow = new BrowserWindow({width: 1050, height: 600});
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({ width: 1050, height: 800, resizable: false });
   mainWindow.loadURL('https://google.es');
 
   // mainWindow.on('closed', function() {
   //   mainWindow = null;
   // });
+  // win.setResizable(false)
 });
